0a68423c00c684dc35642b3f3af1532b886af388
krishna.desai <krishna.desai@CPX-5VC45OJ6T1P>
2018-03-18 00:43:19 +0100

    added few more lines, staged and commit

--- FirstGitHub/FirstGitHub/Program.cs
+++ FirstGitHub/FirstGitHub/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,8 @@ namespace FirstGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("updating program.cs and updating to github");
-            Console.WriteLine("updating program.cs from VS code and pusing it to github");
+            Console.WriteLine("updating program.cs from VS Code and pusing it to github");
+            Console.WriteLine("updating program.cs from VS Code, staging and commit");            
         }
     }
 }
